4fd01c8e5844af18f9d6d75d3921c13aca02d7d0
DanhNguyennene <danhvm12345@gmail.com>
2024-11-01 15:17:26 +0700

    adjust book card

--- frontend/src/components/shop/ShopGrid.jsx
+++ frontend/src/components/shop/ShopGrid.jsx
@@ -1,6 +1,6 @@
 // src/components/shop/ShopGrid.jsx
 import React from 'react';
-import ShopBookCard from './ShopBookCard';
+import ShopBookCard from '../BookCards';
 
 const ShopGrid = ({ books }) => (
     <div className="shop-grid grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
